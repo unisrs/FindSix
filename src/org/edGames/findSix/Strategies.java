061a24b95082782441b236f09464309387bac961
Collective Interaction Research Group <unisrs@gmail.com>
2012-05-21 15:32:39 -0500

    Updated strategies

--- src/org/edGames/findSix/Strategies.java
+++ src/org/edGames/findSix/Strategies.java
@@ -3,6 +3,7 @@ package org.edGames.findSix;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class Strategies extends Activity {
 	/** Called when the activity is first created. */
 	@Override
